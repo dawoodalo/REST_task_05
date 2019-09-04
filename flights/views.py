d0ce7f07944686a511baaf3b743457cdab8d347d
Dawood AlOthman <Dawood@Dawoods-macbook.local>
2019-09-04 14:37:29 +0300

    done

--- flights/views.py
+++ flights/views.py
@@ -2,7 +2,7 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdate
 from datetime import datetime
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer, AUpdateBookingSerializer
 
 
 class FlightsList(ListAPIView):
@@ -11,8 +11,10 @@ class FlightsList(ListAPIView):
 
 
 class BookingsList(ListAPIView):
-	queryset = Booking.objects.filter(date__gte=datetime.today())
 	serializer_class = BookingSerializer
+
+	def get_queryset(self):
+		return Booking.objects.filter(user=self.request.user, date__gte=datetime.today())
 
 
 class BookingDetails(RetrieveAPIView):
@@ -22,11 +24,6 @@ class BookingDetails(RetrieveAPIView):
 	lookup_url_kwarg = 'booking_id'
 
 
-class UpdateBooking(RetrieveUpdateAPIView):
-	queryset = Booking.objects.all()
-	serializer_class = UpdateBookingSerializer
-	lookup_field = 'id'
-	lookup_url_kwarg = 'booking_id'
 
 
 class CancelBooking(DestroyAPIView):
@@ -36,7 +33,7 @@ class CancelBooking(DestroyAPIView):
 
 
 class BookFlight(CreateAPIView):
-	serializer_class = UpdateBookingSerializer
+	serializer_class = AUpdateBookingSerializer
 
 	def perform_create(self, serializer):
 		serializer.save(user=self.request.user, flight_id=self.kwargs['flight_id'])
@@ -44,3 +41,15 @@ class BookFlight(CreateAPIView):
 
 class Register(CreateAPIView):
 	serializer_class = RegisterSerializer
+
+
+class UpdateBooking(RetrieveUpdateAPIView):
+	queryset = Booking.objects.all()
+	lookup_field = 'id'
+	lookup_url_kwarg = 'booking_id'
+
+	def get_serializer_class(self):
+		if self.request.user.is_staff:
+			return AUpdateBookingSerializer
+		else:
+			return UpdateBookingSerializer
